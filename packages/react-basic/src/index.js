fa7c863a69d590f8fe0365d9b7ec89349b12e23d
liuzhijian <18576746147@163.com>
2024-10-25 17:10:13 +0800

    feat: 引入路由

--- packages/react-basic/src/index.js
+++ packages/react-basic/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import routes from './router'
+import {RouterProvider} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={routes}/>
+  </React.StrictMode>
+);
+
