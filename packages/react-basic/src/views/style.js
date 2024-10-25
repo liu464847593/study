b8a709157ecf2b46d37f5807f1df548a12aeb8fd
liuzhijian <18576746147@163.com>
2024-10-25 17:41:47 +0800

    feat: 样式写法

--- packages/react-basic/src/views/style.js
+++ packages/react-basic/src/views/style.js
@@ -3,10 +3,12 @@ const spanStyle = {
   color: 'red',
   fontSize: '50px'
 }
+const flag = true
 const Style = () => {
   return <div>
     <span style={spanStyle}>this is span</span>
     <span className="foo">this is span</span>
+    <span className={`foo ${flag && 'active'}`}>this is span</span>
   </div>
 }
 export default Style
